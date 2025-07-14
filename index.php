a9f2dad648a42b04688a0973aa0ca2d965c96ef8
irdans <irdan.firdaus32@smk.belajar.id>
2025-07-14 13:43:57 +0700

    menambah paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Coba coba</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis id debitis est eos. Asperiores placeat numquam ex pariatur sed id quas consequuntur! Quam dicta delectus asperiores excepturi doloremque, nemo cum?</p>
 </body>
 </html>
