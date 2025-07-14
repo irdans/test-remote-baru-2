32a605576fe60cb383b160561a4bcc73719584ea
irdans <irdan.firdaus32@smk.belajar.id>
2025-07-14 13:48:05 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Coba coba</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis id debitis est eos. Asperiores placeat numquam ex pariatur sed id quas consequuntur! Quam dicta delectus asperiores excepturi doloremque, nemo cum?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
