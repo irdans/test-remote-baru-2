cdb6401c277d0fb2cef80990cfcc12c053b9ebce
irdans <irdan.firdaus32@smk.belajar.id>
2025-07-16 10:04:51 +0700

    Mengubah index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Coba coba</h1>
+    <h2>Coba coba</h2>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis id debitis est eos. Asperiores placeat numquam ex pariatur sed id quas consequuntur! Quam dicta delectus asperiores excepturi doloremque, nemo cum?</p>
     <ul>
         <li>satu</li>
